0c63d702767979b1a353a27f3c2e172586343093
Il <d@fermiumlabs.com>
2016-01-06 19:32:57 +0100

    aggiunto script

---
+++ script.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+echo "################## Cleaning & Building";
+ino clean
+ino build
+
+read -p "################## Upload? [y/n]?" CONT
+if [ "$CONT" == "y" ]; then
+  echo "################## Uploading";
+  ino upload -p /dev/tty.SLAB_USBtoUART;
+else
+  echo "################## skipping upload";
+  exit 0;
+fi
+
+read -p ""################## Open serial terminal? [y/n]?" CONT
+if [ "$CONT" == "y" ]; then
+  echo "################## Opening picocom";
+  ino serial -p /dev/tty.SLAB_USBtoUART;
+else
+  echo "################## skipping serial terminal";
+fi
+
+
+exit 0

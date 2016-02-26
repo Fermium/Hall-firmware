0dadca44a87aeab18607f696d1efee175f954daa
Il <d@fermiumlabs.com>
2016-02-26 02:32:38 +0100

    calibrated

--- lib/Calibration/calibration_hall.h
+++ lib/Calibration/calibration_hall.h
@@ -16,7 +16,7 @@ float CAL_SHUNT_RESISTOR               = 100.0;
 float CAL_VOLTAGE_REFERENCE            = 5.0;     //adc voltage reference
 float CAL_FIXED_GAIN_VRES              = 0.5;     //Opamp gain * voltage divider gain, from 0.1 to 999
 float CAL_FIXED_GAIN_VHALL             = 1.0;     //Opamp gain * voltage divider gain, from 0.1 to 999
-float CAL_HALL_ZERO_VOLTAGE            = 2.5;
+float CAL_HALL_ZERO_VOLTAGE            = 2.678;
 #define SAMPLE_TYPE                    "Ge P"
 unsigned int ADC_RESOLUTION            = 4096;
 

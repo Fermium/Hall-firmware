3421faeffaf4a5b3624fc190c766e5d16d0f8cd1
simone.tosato <simone.tosato95@gmail.com>
2016-03-14 21:35:54 +0100

    Modified ADC to mean readings

--- lib/MCP3304/MCP3304.cpp
+++ lib/MCP3304/MCP3304.cpp
@@ -5,10 +5,11 @@
 #define ADC_MISO 12    // ADC_MISO
 #define SPI_CLK 13  // Clock
 #define ADC_CS 19
+#define STACK_SIZE 10 //size of stacks maxtrix for reads
 
-void MCP3304::init (void)
-{
-
+void MCP3304::init (void) {
+  static int i=0;
+  static int j=0;
 	//set pin modes
  pinMode(ADC_CS, OUTPUT);
  digitalWrite(SPI_CLK,HIGH); //clock
@@ -19,19 +20,28 @@ void MCP3304::init (void)
  digitalWrite(ADC_CS, LOW);
  delayMicroseconds(50);
  digitalWrite(ADC_CS, HIGH);
+ for(i=0;i<8;i++){
+   stacks[i]=(int*)malloc(STACK_SIZE*sizeof(int));
+   ht_stacks[i][0]=0;
+   ht_stacks[i][1]=0;
+ }
+ for(i=0;i<8;i++){
+   for(j=0;j<STACK_SIZE;j++){
+     stacks[i][j]=0;
+   }
+ }
 }
 
 MCP3304::MCP3304(int CS) {
 init();
 }
 
-void MCP3304::pulse(void)
-{
+void MCP3304::pulse(void) {
   digitalWrite(SPI_CLK, LOW); delayMicroseconds(50);
   digitalWrite(SPI_CLK, HIGH); delayMicroseconds(50);
 }
 
-int MCP3304::read(int channel) {
+int MCP3304::read(int channel,bool mean) {
 
 	init();
 
@@ -77,9 +87,31 @@ int MCP3304::read(int channel) {
 	}
 	digitalWrite(ADC_CS, HIGH);
 	delayMicroseconds(10);
+
+  stack_value(value,channel);
+  if(mean){
+    value=mean_value(channel);
+  }
 	return value;
 }
 
 int MCP3304::getCSPin() {
 	return ADC_CS;
+}
+
+void MCP3304::stack_value(int value,int channel) {
+  stacks[channel][ht_stacks[channel][0]]=value;
+  ht_stacks[channel][0]=(ht_stacks[channel][0]+1)%STACK_SIZE;
+  ht_stacks[channel][1]=min(ht_stacks[channel][1]++,STACK_SIZE);
+  }
+
+int MCP3304::mean_value(int channel) {
+  float value;
+  int i=0;
+  for(i=0;i<STACK_SIZE;i++){
+    value=(float)stacks[channel][i];
+  }
+  value/=(float)ht_stacks[channel][1];
+  return (int)roundf(value);
+
 }
--- lib/MCP3304/MCP3304.h
+++ lib/MCP3304/MCP3304.h
@@ -11,7 +11,7 @@ class MCP3304
 		MCP3304(int CS);
 
 		//read Single of Pin
-		int read(int pin);
+		int read(int pin,bool mean);
 
 		// Return SS Pinnumber
 		int getCSPin();
@@ -22,8 +22,28 @@ class MCP3304
 		//initialize spi and IC
 		void init();
 
-	private:
+		//mean the value of read
+		int mean_value(int channel);
 
+	private:
+		void stack_value(int value,int channel);
+		int* stacks[8]; // Circual arrays for mean_value calculation
+		int ht_stacks[8][2];
+		/* Heads and Sizes matrix for stacks circualr array in form of:
+									[,0]		[,1]
+									Head		Size
+								+-------|-------+
+		[0,] Pin 0  |	Head0	|	Size0 |
+		[1,] Pin 1	|	Head1	|	Size1 |
+		[2,] Pin 2	|	Head2	|	Size2 |
+		[3,] Pin 3	|	Head3	|	Size3 |
+		[4,] Pin 4	|	Head4	|	Size4 |
+		[5,] Pin 5	|	Head5	|	Size5 |
+		[6,] Pin 6	|	Head6	|	Size6 |
+		[7,] Pin 7	|	Head7	|	Size7 |
+								+-------|-------+
+		*/
 		int ADC_CS;
+
 };
 #endif
